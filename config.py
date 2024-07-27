c4a71ecc4791cbdb3de15f72ffc274aebaa00e45
Beastvenom21 <anshulgarg7777a@gmail.com>
2024-07-27 08:46:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7388203062:AAHmRivG6FgY5-0sJzGLATkN2rClaAl5cZI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Anshulgarg_1")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1020065910")
 
 
 
